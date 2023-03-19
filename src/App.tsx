c6fff8f5f905d33332d6d817b4a4dcc481779a75
lilian <1220795758@qq.com>
2023-03-19 11:31:17 +0800

    起项

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Button } from "@arco-design/web-react"
+import "@arco-design/web-react/dist/css/arco.css"
+
+const App = () => {
+  return <Button type="primary">按钮</Button>
+}
+
+export default App
